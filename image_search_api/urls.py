a3ada770f7e98a92dffab11945b54b120b50a911
Fasikawkn <fasikawkn@gmail.com>
2022-04-11 14:07:45 +0300

    update

--- image_search_api/urls.py
+++ image_search_api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import re_path as url
-from .views import FileView
+from .views import FileView, TrainImage
+
 
 urlpatterns = [
     url(r'^upload/$', FileView.as_view(), name='file-upload'),
+    url(r'^train/$', TrainImage.as_view(), name='image-train'),
 ]
--- image_search_api/views.py
+++ image_search_api/views.py
@@ -4,13 +4,19 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import FileSerializer
 from .models import File
-# import os
+import os
 import numpy as np
 from numpy.linalg import norm
 import joblib as pickle
 from tqdm import tqdm
-
+import math
+from PIL import Image
+import PIL
+from pathlib import Path
 import tensorflow as tf
+import pickle 
+import shutil
+
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import gc
@@ -21,8 +27,8 @@ from sklearn.neighbors import NearestNeighbors
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from sklearn.decomposition import PCA
-
-
+from numpy.lib.function_base import append
+from re import I
 
 
 class FileView(APIView):
@@ -92,7 +98,98 @@ def findImage(my_image):
   return similar_images_array
 
 
+
+class TrainImage(APIView):
+  parser_classes = (MultiPartParser, FormParser)
+
+  def get(self, request, *args, **kwargs):
+    return Response(data="image",status=status.HTTP_200_OK)
+  
+
+  def post(self, request, *args, **kwargs ):
+    file_serializer = FileSerializer(data=request.data)
+    delete_model()
+    if file_serializer.is_valid():
+      file_serializer.save()
+
+      print("Imag is",file_serializer.data['file'])
+      image = file_serializer.data['file'][1:]
+      print(image)
+
+      move_image(image)
+
+     
+
+      
+      # similar_images = findImage('./'+file_serializer.data['file'])
+      # data = 'http://localhost:8000'+file_serializer.data['file']
+      img_size =224
+      model = ResNet50(weights='imagenet', include_top=False,input_shape=(img_size, img_size, 3),pooling='max')
+      batch_size = 64
+      root_dir = 'media/new_one'
+
+      img_gen = ImageDataGenerator(preprocessing_function=preprocess_input)
+      datagen = img_gen.flow_from_directory(root_dir,
+                                              target_size=(img_size, img_size),
+                                              batch_size=batch_size,
+                                              class_mode=None,
+                                              shuffle=False)
+      num_images = len(datagen.filenames)
+      num_epochs = int(math.ceil(num_images / batch_size))
+      feature_list = model.predict_generator(datagen, num_epochs)
+      create_dir()
+      model.save('saved_model/my_model') 
+      neighbors = NearestNeighbors(n_neighbors=5,
+                             algorithm='ball_tree',
+                             metric='euclidean')
+      neighbors.fit(feature_list)
+      save_model(neighbors)
+      # model.save()
+      print("Num images   = ", len(datagen.classes))
+      print("Shape of feature_list = ", feature_list.shape)
+     
+      return Response(file_serializer.data['file'], status=status.HTTP_201_CREATED)
+
+    else:
+      print('file serializer error', file_serializer.errors)
+      return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+   
+    # return Response(data=True,status=status.HTTP_201_CREATED)
+
  
 
 
+
+def move_image(image):
+  if os.path.exists(image):
+    shutil.move(image, 'media/new_one/Faces')
+    print("The file is moved")
+  else:
+    print('File does not exist')
+
+
+
+def delete_model():
+  shutil.rmtree('saved_model')
+
+
+def save_model(neighbors):
+  #Its important to use binary mode 
+  knnPickle = open('saved_model/neighbors.pkl', 'wb') 
+
+  # # # source, destination 
+  pickle.dump(neighbors, knnPickle)   
+  knnPickle.close();      
+  print('neighbor dumped')
+
+
+
+#creates a directory without throwing an error
+def create_dir():
+  if not os.path.exists('saved_model'):
+    os.makedirs('saved_model')
+    print("Created Directory : ", 'saved_model')
+  else:
+    print("Directory already existed : ", 'saved_model')
+  return dir
 
